05a67fd581da765b7570b7ce669681bcc84253c0
howdy-cdn-sync <cdn-sync@users.noreply.github.com>
2026-08-04 17:24:48 +0000

    cdn: sync _next/static

---
+++ _next/static/chunks/app/(app)/review/loading-d0c3bc1a014a3e7b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3794,1931,4721,9147,3874,2781,9155,4363,1141,5396,512,4999,4977,3015,5666,5169],{63521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
