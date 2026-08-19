5bbd62a2cf9de07715d168f1c68e166a77cebf0d
howdy-cdn-sync <cdn-sync@users.noreply.github.com>
2026-08-19 13:06:22 +0000

    cdn: sync _next/static

---
+++ _next/static/chunks/app/(app)/champions/loading-e4990780b8ff11ae.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2781,1931,4721,9147,5120,3874,9155,4363,5396,1141,4999,512,4977,3794,3015,5666,5169],{63521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
