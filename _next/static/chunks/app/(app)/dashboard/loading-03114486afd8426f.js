816b1f1b07c7e16709ca70d1934cb1d9db0b8122
howdy-cdn-sync <cdn-sync@users.noreply.github.com>
2026-08-19 22:24:19 +0000

    cdn: sync _next/static

---
+++ _next/static/chunks/app/(app)/dashboard/loading-03114486afd8426f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1141,1931,4721,9147,5120,3874,2781,9155,4363,5396,4999,512,3794,4977,5169,5666,3015],{63521:function(){}},function(n){n.O(0,[2971,2117,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
